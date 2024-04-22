6accf39dac97b7f2485f383d48373b9119477674
Grazewal Singh <grazewal-s001@webreinvent.com>
2024-04-22 18:00:01 +0530

    added: button component
    created button component, props and button page

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        <NuxtWelcome />
+        <nav class="py-4 px-2 bg-emerald-100 flex items-center ">
+            <Icon name="ep:menu" />
+            <b class="px-1 text-lg">Custom Components Webreinvent</b>
+        </nav>
+        <div class="w-[260px] h-screen overflow-scroll fixed  bg-yellow-100 flex flex-col gap-1">
+            <router-link to="/button">
+                <Button severity="contrast" link label="Button" />
+            </router-link>
+        </div>
     </div>
 </template>
+<script setup lang="ts">
+
+const handleClick = (event: MouseEvent) => {
+    console.log('Button Clicked', event)
+}
+</script>
