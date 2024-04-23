7f87fdb0552c1cca8d155515460ec6bab61b8460
Grazewal Singh <grazewal-s001@webreinvent.com>
2024-04-23 10:21:50 +0530

    feature -> develop | Added: unit testing for button and radio button

---
+++ project/tests/button.nuxt.test.js
@@ -0,0 +1,340 @@
+import {mount} from "@vue/test-utils";
+import Button from "../components/Button/Button.vue";
+
+
+describe('Custom button test cases for functionality', function () {
+    test('check if component is being imported in line',async()=>{
+        const comp=  await import('../components/Button/Button.vue')
+        expect(comp).toBeDefined()
+    })
+    test('check if component is being mounted',async()=>{
+        const comp= await mount(Button)
+        expect(comp).toBeDefined()
+    })
+    test('label is being rendered', () => {
+        const label = 'Button';
+        const comp = mount(Button, {
+            propsData: {
+                label: label
+            }
+        });
+        const tag=comp.find('#label')
+        expect(tag.text()).toContain(label);
+    });
+    test('button is being clicked ', async() => {
+        const label = 'Button';
+        const comp =await mount(Button);
+
+        const button=  comp.find('[data-testId="button"]').trigger('click')
+
+        expect(button).toBeTruthy()
+    });
+
+    test('emitting event on click when disable', async() => {
+        const label = 'Button';
+        const comp =await mount(Button,{
+            propsData:{
+                label,
+                disabled:true
+            }
+        });
+        const button=await comp.find("button").trigger('click')
+        expect(button).toBeFalsy()
+
+
+    });
+    test('emitting handle-click on button click ', async() => {
+        const label = 'Submit';
+        const comp =await mount(Button,{
+            propsData:{
+                label:label,
+                type:'submit'
+            }
+        });
+        comp.find("button").trigger('click')
+
+        const emiter=await comp.emitted('handle-click')
+
+        expect(emiter).toBeTruthy()
+    });
+
+    test('form with type submit on enter button clicked',async()=>{
+        const label = 'Submit';
+        const comp =await mount(Button,{
+            propsData:{
+                label:label,
+                type:'submit'
+            }
+        });
+
+        const button = comp.find('[data-testId="button"]')
+        expect(button.attributes('type')).toBe('submit');
+
+    })
+
+    test('check if button is showing loader when loading is true',async()=>{
+        const val='Loading'
+        const comp =await mount(Button,{
+            propsData:{
+                severity:'primary',
+                loading:true,
+                label:val
+
+            }
+        });
+        let button=await comp.find('#Loading_text')
+
+        expect(button.text()).toContain(val)
+
+    })
+
+
+});
+
+
+describe('Custom button test cases for background, text and hover color', function () {
+
+    test('check if button are raised on use of raised prop ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'label',
+                raised:true
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+        expect(button.classes()).toContain('shadow');
+    })
+    test('check if button are circular on use of rounded prop ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                rounded:true
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('rounded-full');
+    })
+    test('check if button background is teal  in case of primary severity ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Primary',
+                severity:'primary'
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('bg-emerald-400');
+        expect(button.classes()).toContain('hover:bg-emerald-500')
+        expect(button.classes()).toContain('text-white')
+    })
+
+    test('check if button background is red  in case of danger severity ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Danger',
+                severity:'danger'
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('bg-red-400');
+        expect(button.classes()).toContain('hover:bg-red-500')
+        expect(button.classes()).toContain('text-white')
+    })
+    test('check if button background is orange  in case of warning severity ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Warning',
+                severity:'warning'
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('bg-orange-400');
+        expect(button.classes()).toContain('hover:bg-orange-500')
+        expect(button.classes()).toContain('text-white')
+    })
+    test('check if button background is green  in case of success severity ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Success',
+                severity:'success'
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('bg-green-400');
+        expect(button.classes()).toContain('hover:bg-green-500')
+        expect(button.classes()).toContain('text-white')
+    })
+
+    test('check if button background is blue  in case of info severity ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Information',
+                severity:'info'
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('bg-blue-400');
+        expect(button.classes()).toContain('hover:bg-blue-500')
+        expect(button.classes()).toContain('text-white')
+    })
+
+    test('check if button background is purple  in case of help severity ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Help',
+                severity:'help'
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('bg-purple-400');
+        expect(button.classes()).toContain('hover:bg-purple-500')
+        expect(button.classes()).toContain('text-white')
+    })
+
+    test('check if outlined is being applied ',async()=>{
+        const comp =await mount(Button,{
+                propsData:{
+                    label:'Help',
+                    severity:'help',
+                    outlined:true
+
+                }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('border');
+        expect(button.classes()).toContain('hover:bg-purple-50')
+        expect(button.classes()).toContain('text-purple-600')
+    })
+    test('check if bg-hover effect is being applied ',async()=>{
+        const comp =await mount(Button,{
+            propsData:{
+                label:'Help',
+                severity:'help',
+                outlined:true
+
+            }
+        });
+        let button=comp.find('[data-testId="button"]')
+
+        expect(button.classes()).toContain('border');
+        expect(button.classes()).toContain('hover:bg-purple-50')
+        expect(button.classes()).toContain('text-purple-600')
+    })
+});
+describe('Custom styles',()=>{
+    test('check if custom style using inline style is being applied', async function(assert) {
+        const customStyle='background-color: indianred; border-radius: 50%;'
+            const comp= await mount(Button,{
+                props:{
+                    styleName:customStyle
+                }
+            })
+        await comp.vm.$nextTick();
+        const buttonElement = comp.find('[data-testId="button"]');
+        expect(buttonElement.attributes('style')).toEqual(customStyle);
+    });
+    test('check if custom className  is being applied', async function(assert) {
+        let customClass='bg-pink-200'
+        const comp= await mount(Button,{
+            props:{
+                className: customClass
+            }
+        })
+        await comp.vm.$nextTick();
+        const button = comp.find('[data-testId="button"]');
+        expect(button.classes()).toContain(customClass)
+    });
+
+})
+
+describe('Button Accessibility', () => {
+    let comp;
+
+    beforeEach(() => {
+        comp = mount(Button, {
+            attachTo: document.body,
+            props: {
+                label: 'Submit'
+            },
+
+
+        });
+    });
+
+    afterEach(() => {
+        comp.unmount();
+    });
+
+
+    test('should be focusable using keyboard navigation', async () => {
+        const wrapper = await mount(Button)
+        const button=await wrapper.find('button')
+         await  button.trigger('keydown.tab');
+
+
+        expect(document.activeElement).not.toBe(button);
+    });
+
+    test('should have proper aria attributes for accessibility', () => {
+        const button = comp.find('[data-testId="button"]');
+        expect(button.attributes('role')).toBe('button');
+        expect(button.attributes('aria-label')).toBe('Submit');
+        expect(button.attributes('aria-disabled')).toBe('false');
+    });
+
+    test('should work with screen readers', () => {
+        const button = comp.find('[data-testId="button"]');
+        expect(button.attributes('aria-hidden')).toBeUndefined();
+        expect(button.attributes('role')).toBe('button');
+        expect(button.text()).toBe('Submit');
+    });
+});
+
+describe('Button Performance', () => {
+    let comp;
+
+    beforeEach(() => {
+        comp = mount(Button);
+    });
+
+    afterEach(() => {
+        comp.unmount();
+    });
+
+    test('loading time should be within acceptable limits', () => {
+        const startTime = performance.now();
+        mount(Button);
+        const endTime = performance.now();
+        const loadingTime = endTime - startTime;
+        expect(loadingTime).toBeLessThan(100);
+    });
+
+    test('responsiveness should be within acceptable limits', () => {
+        const button = comp.find('[data-testId="button"]');
+        const startTime = performance.now();
+        button.trigger('click');
+
+        return new Promise(resolve => {
+            setTimeout(() => {
+                const endTime = performance.now();
+                const responseTime = endTime - startTime;
+                expect(responseTime).toBeLessThan(600);
+                resolve();
+            }, 500);
+        });
+    });
+
+
+});
---
+++ project/tests/radioButton.nuxt.test.js
@@ -0,0 +1,228 @@
+import {mount} from "@vue/test-utils";
+import RadioButton from "../components/RadioButton/RadioButton.vue";
+
+
+describe('Custom button test cases for functionality', function () {
+    test('check if component is being imported in line',async()=>{
+        const comp=  await import('../components/RadioButton/RadioButton.vue')
+        expect(comp).toBeDefined()
+    })
+    test('check if component is being mounted',async()=>{
+        const comp= await mount(RadioButton)
+        expect(comp).toBeDefined()
+    })
+    test('label is being rendered', () => {
+        const label = 'Button';
+        const comp = mount(RadioButton, {
+            propsData: {
+                name: label
+            }
+        });
+        const tag=comp.find('#label')
+        expect(tag.text()).toContain(label);
+    });
+    test('button is being clicked ', async() => {
+         
+        const comp =await mount(RadioButton);
+
+        const button=  comp.trigger('click')
+
+        expect(button).toBeTruthy()
+    });
+     
+    test('input border  disabled when disabled is passed',async()=>{
+        const comp = await mount(RadioButton,{
+            propsData:{
+            inputId:"radios",
+            disabled:true
+            }
+        })
+         
+        const input= comp.find("#radios")
+        
+        expect(input.element.disabled).toBeTruthy()
+        
+    })
+
+});
+
+describe("Reacting when selected ",async()=>{
+    const comp = await mount(RadioButton,{
+            propsData:{
+            inputId:"radios"
+            }
+        })
+
+    test("when clicked checked status is being change of the radio button",async()=>{
+        const input= comp.find("#radios")
+        
+        expect(input.element.checked).toBeFalsy()
+        comp.trigger('click')
+
+       
+        expect(input.element.checked).toBeTruthy()
+        
+    }
+    
+)
+    test("Input box is scaling up when clicked",async()=>{
+        comp.trigger('click')
+        const input= comp.find("#radios")
+        expect(input.element.classList).toContain('checked:scale-100')
+    })
+    test('input border is changing when hover',async()=>{
+       
+        const input= comp.find("#inputWrapper")
+        expect(input.element.classList).toContain('border-gray-300')
+        comp.trigger('hover')
+        expect(input.element.classList).toContain('hover:border-gray-400')
+    })
+
+    test('input border is changing when value is matched',async()=>{
+       
+        const comp = await mount(RadioButton,{
+            propsData:{
+            inputId:"radios",
+            modelValue: "kjbd",
+            value:"Abc"
+            },
+        })
+        const inputWrapper= comp.find("#inputWrapper")
+        
+        expect(inputWrapper.element.classList).toContain('border-gray-300')
+         
+        const comp2 = await mount(RadioButton,{
+            propsData:{
+            inputId:"radios",
+            modelValue: "Abc",
+            value:"Abc"
+            },
+        })
+        const inputWrapper2= comp2.find("#inputWrapper")
+          
+        expect(inputWrapper2.element.classList).toContain('border-emerald-400')
+        
+    })
+
+    test('input border is red and scale and bg is none when invalid',async()=>{
+        const comp = await mount(RadioButton,{
+            propsData:{
+            inputId:"radios",
+            invalid:true
+            }
+        })
+         
+      
+        
+        const inputWrapper= comp.find("#inputWrapper")
+            const input=await comp.find("#radios")
+ 
+        expect(input.element.classList).toContain('bg-none')
+
+        expect(input.element.classList).toContain('scale-0')
+        expect(inputWrapper.element.classList).toContain('border-red-400')
+        
+    })
+    test('input border and bg is gray when disbaled is passed',async()=>{
+        const comp = await mount(RadioButton,{
+            propsData:{
+            inputId:"radios",
+            disabled:true
+            }
+        })
+      
+        
+        const inputWrapper= comp.find("#inputWrapper")
+        const input=comp.find("#radios")
+        expect(input.element.classList).toContain('bg-gray-400')
+    
+        
+        expect(inputWrapper.element.classList).toContain('border-gray-400')
+        
+    })
+
+    test('check if slot is rendering the data',async()=>{
+            const comp= await mount(RadioButton,{
+                slots:{
+                    name:`<template>radio</template>`
+                }
+            })
+            expect(comp.html()).toContain(`<template>radio</template>`)
+           
+    })
+
+    test("Input box is changing color when clicked",async()=>{
+        const input= comp.find("#radios")
+
+        comp.trigger('click')
+        
+        expect(input.element.classList).toContain('checked:bg-emerald-400')
+    })
+})
+
+ 
+
+describe('Button Accessibility', () => {
+    let comp;
+
+    beforeEach(() => {
+        comp = mount(RadioButton, {
+            attachTo: document.body,
+            props: {
+                name: 'Submit',
+                inputId:"Radio1"
+            },
+
+
+        });
+    });
+
+    afterEach(() => {
+        comp.unmount();
+    });
+
+    test('should have proper aria attributes for accessibility', () => {
+        const button = comp.find('#Radio1');
+        expect(button.attributes('role')).toBe('inputRadio');
+        expect(button.attributes('aria-label')).toBe('Submit');
+        expect(button.attributes('aria-disabled')).toBe('false');
+    });
+
+    test('should work with screen readers', () => {
+        const button = comp.find('#Radio1');
+        expect(button.attributes('aria-hidden')).toBeUndefined();
+        expect(button.attributes('role')).toBe('inputRadio');
+        
+    });
+});
+
+describe('Button Performance', () => {
+    let comp;
+
+    beforeEach(() => {
+        comp = mount(RadioButton);
+    });
+
+    afterEach(() => {
+        comp.unmount();
+    });
+
+      
+
+    test('responsiveness should be within acceptable limits', () => {
+        
+        const startTime = performance.now();
+        comp.trigger('click');
+
+        return new Promise(resolve => {
+            setTimeout(() => {
+                const endTime = performance.now();
+                const responseTime = endTime - startTime;
+                expect(responseTime).toBeLessThan(650);
+                resolve();
+            }, 500);
+        });
+    });
+
+
+});
