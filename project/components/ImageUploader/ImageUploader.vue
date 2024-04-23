03cee04c16f346f2feb072bb0c384819bc5d5c23
Grazewal Singh <grazewal-s001@webreinvent.com>
2024-04-23 18:45:57 +0530

    added: Drag and drop for image uploading

--- project/components/ImageUploader/ImageUploader.vue
+++ project/components/ImageUploader/ImageUploader.vue
@@ -1,71 +1,118 @@
 <template>
 
 
-  <input @change="handleChange" ref="inputRef"  type="file" class="scale-0 absolute" accept="image/*" :multiple="props.multiple" />
+  <input @change="handleChange" ref="inputRef" type="file" class="scale-0 absolute" accept="image/*"
+         :multiple="props.multiple"/>
   <div v-if="props.mode=='basic'" :class="` ${inputClass} `" @click=" upload ? uploadFiles():handleChoose()">
-    <Icon class="mx-1" :name="`${upload?'material-symbols:add':'material-symbols:upload-sharp'}`"/>
-    <span>{{upload?image[0].name:props.chooseLabel}}</span>
+    <Icon size="20" class="mx-1" :name="`${upload?'material-symbols:add':'material-symbols:upload-sharp'}`"/>
+    <span>{{ upload ? image[0].name : props.chooseLabel }}</span>
   </div>
   <div v-else>
+    <div class="flex gap-2 py-2">
+      <slot name="header">
+        <Button severity="primary" iconPos="left" icon="material-symbols:add" label="Choose"
+                @handle-click="handleChoose"/>
+        <Button severity="primary" iconPos="left" icon="material-symbols:upload-sharp" label="Upload"
+                @handle-click="uploadFiles"/>
+        <Button severity="primary" iconPos="left" icon="material-symbols:close-rounded" label="Cancel"
+                @handle-click="handleCancel"/>
+      </slot>
+    </div>
 
-    <slot name="empty">
-
-    </slot>
-
-    <slot name="header"/>
-    <slot name="content"/>
-
+    <div @dragover.prevent="handleDragOver"
+         @dragenter.prevent="handleDragEnter"
+         @dragleave.prevent="handleDragLeave"
+         @drop.prevent="handleDrop"
+         :class="dropBoxClass">
+      <slot v-if="image?.length>0" name="content">
+        <div class="flex my-2 border-[1px] border-solid border-gray-400 p-2 rounded-md cursor-pointer"
+             v-for="(file, index) in image" :key="index">
+          <div class='flex gap-2 justify-between'>
+            <img v-if="file.type.startsWith('image/')" :src="getImageUrl(file)" alt="Preview" class="w-16  rounded"/>
+            <div>
+              <p>{{ file.name }}</p>
+              <p>{{ file.size }}</p>
+            </div>
+          </div>
+          <Button @handle-click="deleteImage(index)" iconColor="red" className="w-8 h-8 hover:bg-transparent"
+                  icon="material-symbols:close-rounded" rounded/>
+        </div>
+      </slot>
+      <slot v-else name="empty">
+      </slot>
+    </div>
   </div>
-
-
-
 
 </template>
 
 <script setup lang="ts">
 
-import { Props } from './props';
+import {Props} from './props';
 import {twMerge} from "tailwind-merge";
 
 const props = defineProps(Props);
 const image: Ref<FileList | null> = ref(null);
+const imageUrl = ref(null)
 const inputRef: Ref<HTMLInputElement | null> = ref(null);
 const emit = defineEmits();
-const loading= ref(false)
-const upload=ref(false)
+const loading = ref(false)
+const upload = ref(false)
 
-const basicStyle='bg-emerald-500 hover:bg-emerald-600 p-2 rounded-md min-w-24 cursor-pointer h-10  flex items-center gap-1 text-white '
+const dragging = ref(false)
 
-const inputClass=twMerge(` ${loading.value?'opacity-50':''} relative ${basicStyle} inline-block`.split(" "))
-const error: Ref<String | null> =ref()
+
+const basicStyle = 'bg-emerald-400 hover:bg-emerald-500 p-2 rounded-md min-w-24 cursor-pointer h-10  flex items-center gap-1 text-white '
+
+
+const inputClass = twMerge(` ${loading.value ? 'opacity-50' : ''} relative ${basicStyle} inline-block`.split(" "))
+const dropBoxClass = computed(() => twMerge(`p-4 border-dashed border-2
+${dragging.value || image.value?.length > 0 ? ' !border-emerald-500 ' : ' border-gray-400'} `.split(" ")))
+const error: Ref<String | null> = ref()
 
 const handleChoose = () => {
   if (inputRef.value) {
     inputRef.value.click();
   }
 };
+const deleteImage = (index: number) => {
+  const newArray = Array.from(image.value);
+  newArray.splice(index, 1);
+  image.value = newArray;
+};
 
-function handleChange(e: Event){
+function handleChange(e: Event) {
+
   const target = e.target as HTMLInputElement;
-
   if (target.files && target.files.length > 0) {
-    image.value = target.files;
-    if(props.auto){
-      uploadFiles()
-    }else{
-      upload.value=true
+    const newFiles = target.files;
+    if (image.value) {
+      const allFiles = Array.from(image.value).concat(Array.from(newFiles));
 
+      const fileArray: File[] = allFiles.map(file => file instanceof File ? file : file instanceof Blob ? new File([file], file.name) : file);
+      const newFileList = new DataTransfer();
+      fileArray.forEach(file => {
+        newFileList.items.add(file);
+      });
+
+      image.value = newFileList.files;
+    } else {
+     
+      image.value = target.files;
     }
 
 
-    console.log(image.value[0].name)
+    if (props.auto) {
+      uploadFiles()
+    } else {
+      upload.value = true
+    }
   }
 };
 
 
-const uploadFiles = async () => {
-console.log("Upload function is being called")
-loading.value=true
+async function uploadFiles() {
+  console.log("Upload function is being called")
+  loading.value = true
   try {
     const apiUrl: string = props.url;
     const formData = new FormData();
@@ -78,15 +125,42 @@ loading.value=true
       body: formData,
     });
 
-    emit('handle-response',response);
-    loading.value=false
+    emit('handle-response', response);
+    loading.value = false
   } catch (err) {
-   error.value='Error uploading files!'
-    loading.value=false
+    error.value = 'Error uploading files!'
+    loading.value = false
   }
 };
 
+function handleDragOver(e: DragEvent) {
+  e.preventDefault()
+  dragging.value = true;
+}
 
+function handleDragEnter(e: DragEvent) {
+  e.preventDefault();
+  dragging.value = true;
+}
+
+function handleDragLeave(e: DragEvent) {
+  dragging.value = false;
+}
+
+const getImageUrl = (file: File) => {
+  return URL.createObjectURL(file);
+};
+
+function handleDrop(e: DragEvent) {
+  e.preventDefault();
+  console.log(e.target, 'link')
+  dragging.value = false;
+  const files = e.dataTransfer?.files;
+  if (files && files.length > 0) {
+    image.value = files;
+    upload.value = true;
+  }
+}
 </script>
 
 <style scoped>
@@ -100,8 +174,9 @@ loading.value=true
   cursor: pointer;
 
 }
+
 .error {
-  color:red;
+  color: red;
   opacity: 0;
   animation: fadeIn;
   animation-duration: .5s;
@@ -109,7 +184,11 @@ loading.value=true
 }
 
 @keyframes fadeIn {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
--- project/pages/ImageUploaderPage.vue
+++ project/pages/ImageUploaderPage.vue
@@ -10,8 +10,11 @@
   <div class="p-2 bg-gray-200 rounded mt-2">
     <b>Image uploader drag and drop </b>
     <div class="m-2 flex gap-1 flex-wrap">
-      <image-uploader   @handle-response="getResponse"  url="https://uploadcare.com/docs/uploads/"/>
-      <image-uploader   chooseLabel="Auto Upload" @handle-response="getResponse"  url="https://uploadcare.com/docs/uploads/"/>
+      <image-uploader  name="demo[]" url="/api/upload" @upload="onAdvancedUpload($event)" :multiple="true" accept="image/*" :maxFileSize="1000000">
+        <template #empty>
+          <p>Drag and drop files to here to upload.</p>
+        </template>
+      </image-uploader >
     </div>
   </div>
 </template>
